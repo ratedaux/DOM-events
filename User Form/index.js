9972199e4c798894ec60ec2d415518967ea3a30b
Kristina Tereshchenko <kristovinia@gmail.com>
2024-12-12 12:32:39 +0100

    js file + html

--- User Form/index.js
+++ User Form/index.js
@@ -4,17 +4,19 @@ document
     event.preventDefault();
 
     const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
+    const surname = document.getElementById("surname").value;
+    const age = document.getElementById("age").value;
+    const position = document.getElementById("position").value;
 
-    if (!name || !email || !phone) {
+    if (!name || !surname || !age || !position) {
       alert("Введите информацию во все обязательные поля");
       return;
     }
 
     document.getElementById("cardName").textContent = name;
-    document.getElementById("cardEmail").textContent = email;
-    document.getElementById("cardPhone").textContent = phone;
+    document.getElementById("cardSurname").textContent = surname;
+    document.getElementById("cardAge").textContent = age;
+    document.getElementById("cardPosition").textContent = position;
 
     document.getElementById("userCard").classList.remove("hidden");
   });
