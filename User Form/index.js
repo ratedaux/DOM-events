18a5419691c99a333ef4e32bdde34d79dbf38e77
Kristina Tereshchenko <kristovinia@gmail.com>
2024-12-14 23:28:41 +0100

    alert + age

--- User Form/index.js
+++ User Form/index.js
@@ -6,23 +6,26 @@ document.getElementById("userForm").addEventListener("submit", (event) => {
   const age = document.getElementById("age").value;
   const position = document.getElementById("position").value;
 
-  // Проверка заполненности всех полей
-  if (!name || !surname || !age || !position) {
+  if (!name || !surname || !age) {
     alert("Введите информацию во все обязательные поля");
     document.getElementById("userCard").classList.add("hidden"); 
     return;
   }
 
-  // Заполнение данных в карточке пользователя
+  if (isNaN(age) || age < 15 || age > 120) {
+    alert("Введите корректный возраст (число от 16 до 120)");
+    document.getElementById("userCard").classList.add("hidden");
+    return;
+  }
+
   document.getElementById("cardName").textContent = name;
   document.getElementById("cardSurname").textContent = surname;
   document.getElementById("cardAge").textContent = age;
   document.getElementById("cardPosition").textContent = position;
 
-  // Показ карточки
   document.getElementById("userCard").classList.remove("hidden");
 
-  // Добавление в список
+  // selectOption
   const userCheck = document.getElementById("userCheck");
   const option = document.createElement("option");
   option.value = JSON.stringify({ name, surname, age, position });
@@ -35,7 +38,6 @@ document.getElementById("userForm").addEventListener("submit", (event) => {
 document.getElementById("userCheck").addEventListener("change", (event) => {
   const selectedValue = event.target.value;
 
-  // Проверка выбора
   if (!selectedValue) {
     document.getElementById("userCard").classList.add("hidden"); 
     return;
@@ -43,21 +45,9 @@ document.getElementById("userCheck").addEventListener("change", (event) => {
 
   const { name, surname, age, position } = JSON.parse(selectedValue);
 
-  // Заполнение данных при выборе
   document.getElementById("cardName").textContent = name || "";
   document.getElementById("cardSurname").textContent = surname || "";
   document.getElementById("cardAge").textContent = age || "";
   document.getElementById("cardPosition").textContent = position || "";
   document.getElementById("userCard").classList.remove("hidden");
-});
-
-// Блокировка кнопки "Создать", если поля пустые
-document.getElementById("userForm").addEventListener("input", () => {
-  const name = document.getElementById("name").value;
-  const surname = document.getElementById("surname").value;
-  const age = document.getElementById("age").value;
-  const position = document.getElementById("position").value;
-
-  const button = document.querySelector("button[type='submit']");
-  button.disabled = !(name && surname && age && position); 
 });
